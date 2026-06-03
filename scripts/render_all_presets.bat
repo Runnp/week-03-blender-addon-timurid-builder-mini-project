4f48f9103ddd7d6d71e3015110fca23bfb44dab1
Runnp / Nurmuhammad <runnp3ddd@gmail.com>
2026-06-03 22:28:47 +0500

    Day Scripts Polish Render

---
+++ scripts/render_all_presets.bat
@@ -0,0 +1,6 @@
+@echo off
+REM render_all_presets.bat
+REM Renders all 4 presets at MID LOD with tile materials to demo_renders/
+REM
+REM Usage: render_all_presets.bat [path\to\blender.exe]
+REM Default blender path assumes it's on PATH.
